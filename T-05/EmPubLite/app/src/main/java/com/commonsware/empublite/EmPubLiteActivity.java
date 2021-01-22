0538f980749d6402caadcdb3f18ac42e87fd1595
Nenartovich <evgeny.nenart@gmail.com>
2021-01-22 15:15:17 +0300

    T-05 Step #0: Init commit

---
+++ T-05/EmPubLite/app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -0,0 +1,14 @@
+package com.commonsware.empublite;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EmPubLiteActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    //test new feature
+  }
+}
