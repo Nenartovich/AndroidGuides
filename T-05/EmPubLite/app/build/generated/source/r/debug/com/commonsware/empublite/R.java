3b5c5a6a25a15a513feb710b17cb643fae9934b9
Nenartovich <evgeny.nenart@gmail.com>
2021-01-22 15:29:26 +0300

    T-05 Step #2: Defining the UI

--- T-05/EmPubLite/app/build/generated/source/r/debug/com/commonsware/empublite/R.java
+++ T-05/EmPubLite/app/build/generated/source/r/debug/com/commonsware/empublite/R.java
@@ -18,16 +18,19 @@ public final class R {
     public static final int activity_vertical_margin=0x7f020001;
   }
   public static final class id {
-    public static final int main=0x7f030000;
+    public static final int editor=0x7f030000;
+    public static final int main=0x7f030001;
   }
   public static final class layout {
-    public static final int main=0x7f040000;
+    public static final int editor=0x7f040000;
+    public static final int main=0x7f040001;
   }
   public static final class mipmap {
     public static final int ic_launcher=0x7f050000;
   }
   public static final class string {
     public static final int app_name=0x7f060000;
+    public static final int hint=0x7f060001;
   }
   public static final class style {
     public static final int AppTheme=0x7f070000;
