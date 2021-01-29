f7a1222bd5de8bd940e152b77f103270f63bf0e7
Evgeniy Nenartovich <nenartovski@yandex.com>
2021-01-29 22:58:07 +0300

    Custom adapter creating

---
+++ Example006_CustomAdapter/app/src/main/java/com/nenartovich/example006_customadapter/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.nenartovich.example006_customadapter
+
+import android.annotation.SuppressLint
+import android.app.ListActivity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : ListActivity() {
+    private val items = arrayOf("lorem", "ipsum", "dolor",
+            "sit", "amet",
+            "consectetuer", "adipiscing", "elit", "morbi", "vel",
+            "ligula", "vitae", "arcu", "aliquet", "mollis",
+            "etiam", "vel", "erat", "placerat", "ante",
+            "porttitor", "sodales", "pellentesque", "augue", "purus")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        listAdapter = IconicAdapter()
+    }
+
+    inner class IconicAdapter() : ArrayAdapter<String>(this@MainActivity, R.layout.row, R.id.label, items) {
+        @SuppressLint("SetTextI18n")
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val row = super.getView(position, convertView, parent)
+            val icon = row.findViewById<ImageView>(R.id.icon)
+            if (items[position].length > 4) {
+                icon.setImageResource(R.drawable.ok)
+            } else {
+                icon.setImageResource(R.drawable.delete)
+            }
+
+            val size = row.findViewById<TextView>(R.id.size)
+            size.text = "${getString(R.string.size_template)} ${items[position].length}"
+            return row
+        }
+    }
+}
