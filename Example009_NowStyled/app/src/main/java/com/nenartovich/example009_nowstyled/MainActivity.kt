8396e22024867b033e8f68eaf4c44ec540d2057c
Evgeniy Nenartovich <nenartovski@yandex.com>
2021-01-31 19:11:21 +0300

    Styles and Themes applying

---
+++ Example009_NowStyled/app/src/main/java/com/nenartovich/example009_nowstyled/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nenartovich.example009_nowstyled
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
