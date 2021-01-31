da8eb6a9d14a55c9b8e0d66b3df16ef7ad7590e8
Evgeniy Nenartovich <nenartovski@yandex.com>
2021-01-31 23:14:57 +0300

    ConstraintLayout sample project

---
+++ Example010_ConstraintLayoutSampleProject/app/src/main/java/com/nenartovich/example010_constraintlayoutsampleproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nenartovich.example010_constraintlayoutsampleproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
