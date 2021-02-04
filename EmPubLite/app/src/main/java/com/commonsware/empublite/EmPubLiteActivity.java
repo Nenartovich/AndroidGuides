25a706b4e471c61b83966cabfea8f661c27cc90b
Evgeniy Nenartovich <nenartovski@yandex.com>
2021-02-04 22:18:15 +0300

    Tutorial #8 - Setting Up An Activity

--- EmPubLite/app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ EmPubLite/app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -1,6 +1,7 @@
 package com.commonsware.empublite;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,8 +24,12 @@ public class EmPubLiteActivity extends Activity {
   public boolean onOptionsItemSelected(MenuItem item) {
     switch (item.getItemId()) {
       case R.id.about:
+        Intent i=new Intent(this, SimpleContentActivity.class);
+        startActivity(i);
         return true;
       case R.id.help:
+        i = new Intent(this, SimpleContentActivity.class);
+        startActivity(i);
         return true;
     }
     return super.onOptionsItemSelected(item);
---
+++ EmPubLite/app/src/main/java/com/commonsware/empublite/SimpleContentActivity.java
@@ -0,0 +1,12 @@
+package com.commonsware.empublite;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class SimpleContentActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
